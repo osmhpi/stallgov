53a938ea91460fe5c54c2e8a8a06dc400fd1b881
Leon Matthes <leon@matthes.biz>
2021-12-16 17:05:12 +0100

    Read performance counter in memutil governor
    Note: Requires a patched kernel with perf_event_read_local exposed to
    modules

--- memutil.c
+++ memutil.c
@@ -8,13 +8,17 @@
 #include <linux/init.h> // included for __init and __exit macros
 #include <linux/sched/cpufreq.h>
 #include <trace/events/power.h>
+#include <linux/perf_event.h>
+#include <linux/err.h>
 
 struct memutil_policy {
 	struct cpufreq_policy *policy;
 
-	raw_spinlock_t	update_lock; /* For shared policies */
-	u64		last_freq_update_time;
-	s64		freq_update_delay_ns;
+	raw_spinlock_t		update_lock; /* For shared policies */
+	u64			last_freq_update_time;
+	s64			freq_update_delay_ns;
+
+	struct perf_event	*perf_event;
 };
 
 struct memutil_cpu {
@@ -27,10 +31,30 @@ struct memutil_cpu {
 
 static DEFINE_PER_CPU(struct memutil_cpu, memutil_cpu_list);
 
-void memutil_set_frequency(struct cpufreq_policy *policy)
+void memutil_set_frequency(struct memutil_policy *memutil_policy)
 {
+
+	int			perf_result;
+	u64			perf_value;
+	u64			enabled_time;
+	u64			running_time;
+	struct cpufreq_policy 	*policy = memutil_policy->policy;
+
+	if(memutil_policy->policy->cpu == 0) {
+		perf_result = perf_event_read_local(
+				memutil_policy->perf_event,
+				&perf_value,
+				&enabled_time,
+				&running_time); 
+
+		if(likely(perf_result == 0)) {
+			pr_info_ratelimited("Perf value %llu", perf_value);
+		} else {
+			pr_info_ratelimited("Perf read failed: %d", perf_result);
+		}
+	}
+
 	if (!policy_is_shared(policy) && policy->fast_switch_enabled) {
-		pr_info_ratelimited("Setting frequency to: %u", policy->min);
 		cpufreq_driver_fast_switch(policy, policy->min);
 	}
 	else {
@@ -60,10 +84,48 @@ static void memutil_policy_free(struct memutil_policy *memutil_policy)
 	kfree(memutil_policy);
 }
 
+static long __must_check memutil_allocate_perf_counter(struct memutil_policy *policy) 
+{
+	struct perf_event_attr perf_attr;
+	struct perf_event *perf_event;
+
+	memset(&perf_attr, 0, sizeof(perf_attr));
+
+	perf_attr.type = PERF_TYPE_HARDWARE;
+	perf_attr.size = sizeof(perf_attr);
+	perf_attr.config = PERF_COUNT_HW_INSTRUCTIONS;
+	perf_attr.disabled = 0; // enable the event by default
+	perf_attr.exclude_kernel = 1;
+	perf_attr.exclude_hv = 1;
+
+	perf_event = perf_event_create_kernel_counter(
+			&perf_attr,
+			policy->policy->cpu,
+			/*task*/ NULL, 
+			/*overflow_handler*/ NULL,
+			/*context*/ NULL);
+
+	if(unlikely(IS_ERR(perf_event))) {
+		return PTR_ERR(perf_event);
+	}
+
+	policy->perf_event = perf_event;
+	return 0;
+}
+
+static void memutil_release_perf_event(struct memutil_policy *policy)
+{
+	if(unlikely(policy->perf_event == NULL)) {
+		pr_warn("Tried to release perf_event which is NULL");
+		return;
+	}
+	perf_event_release_kernel(policy->perf_event);
+}
+
 static int memutil_init(struct cpufreq_policy *policy)
 {
-	struct memutil_policy *memutil_policy;
-	int return_value = 0;
+	struct memutil_policy 	*memutil_policy;
+	int 			return_value = 0;
 
 	printk(KERN_INFO "Loading memutil module");
 
@@ -76,16 +138,27 @@ static int memutil_init(struct cpufreq_policy *policy)
 
 	memutil_policy = memutil_policy_alloc(policy);
 	if (!memutil_policy) {
+		pr_err("Failed to allocate memutil policy!");
 		return_value = -ENOMEM;
 		goto disable_fast_switch;
 	}
 
 	policy->governor_data = memutil_policy;
+
+	return_value = (int) memutil_allocate_perf_counter(memutil_policy);
+	if (unlikely(return_value != 0)) {
+		pr_err("Failed to acquire hardware performance counter");
+		goto free_policy;
+	}
+
 	return 0;
 
+free_policy:
+	memutil_policy_free(memutil_policy);
+	// fallthrough
 disable_fast_switch:
 	cpufreq_disable_fast_switch(policy);
-	pr_err("initialization failed (error %d)\n", return_value);
+	pr_err("memutil_init failed (error %d)\n", return_value);
 	return return_value;
 }
 
@@ -98,6 +171,7 @@ static void memutil_exit(struct cpufreq_policy *policy)
 
 	policy->governor_data = NULL;
 
+	memutil_release_perf_event(memutil_policy);
 	memutil_policy_free(memutil_policy);
 	cpufreq_disable_fast_switch(policy);
 }
@@ -144,7 +218,7 @@ static void memutil_update_single_frequency(struct update_util_data *hook, u64 t
 		return;
 	}
 
-	memutil_set_frequency(memutil_policy->policy);
+	memutil_set_frequency(memutil_policy);
 	memutil_policy->last_freq_update_time = time;
 }
 
