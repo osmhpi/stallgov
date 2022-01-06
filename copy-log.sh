1579019b3227b3d8ad001fff61c3f25ec1426dd6
Erik Griese <erik.griese@student.hpi.de>
2022-01-06 16:29:43 +0100

    Extends copy log bash script with more doc and possibility to clear log

--- copy-log.sh
+++ copy-log.sh
@@ -1,19 +1,39 @@
 #!/bin/bash
 
 usage() {
-    echo "Usage: $0 [-s SOURCE (/sys/kernel/debug/memutil/log) | -i INTERVAL (60)] <output_dir> <core_number>"
+    echo "Usage: $0 [-s SOURCE (/sys/kernel/debug/memutil/log) | -i INTERVAL (8)] <output_dir> <core_number>"
+    echo ""
+    echo "The log of memutil is only large enough for a couple of seconds and is cleared after each read to have space for the next data."
+    echo "This utility script periodically copies the log created by memutil and appends it to log.txt in the given output dir."
+    echo "Also one separate log for each core is created."
+    echo "Parameters:"
+    echo -e "\t-s SOURCE: Specifies the path to the memutil log"
+    echo -e "\t-i INTERVAL: Specifies the interval with which the log should be copied"
+    echo -e "\t-c: Clear the existing log files in the given output dir."
+    echo -e "\t<output_dir>: Directory where the copied log and separate log files should be stored. Has to exist for this command to work"
+    echo -e "\t<core_number>: The amount of cores memutil runs on. This is needed to know how many separate log files have to be created."
 }
 
 copy_log_part() {
     cat $1 | grep "CPU\[$2\]" >> $3/log-$2.txt
 }
 
+clear_logs() {
+    echo "Clearing logs..."
+    rm -f $1/log.txt
+    for i in $(seq 0 1 $(($2-1)))
+    do
+        rm -f $1/log-$i.txt
+    done
+}
+
 [ $# -eq 0 ] && usage && exit 1
 
 SOURCE_FILE=/sys/kernel/debug/memutil/log
-INTERVAL=60
+INTERVAL=8
+SHOULD_CLEAR=false
 
-while getopts ":hs:i:" arg
+while getopts ":hs:i:c" arg
 do
     case $arg in
         s) # Specify source
@@ -21,6 +41,9 @@ do
             ;;
         i) # Specify interval
             INTERVAL=${OPTARG}
+            ;;
+        c) # Clear the existing logs
+            SHOULD_CLEAR=true
             ;;
         h | *) # Display help
             usage
@@ -39,6 +62,18 @@ fi
 OUTPUT_DIR=$1
 CORE_NUMBER=$2
 
+if [ ! -d "$OUTPUT_DIR" ]
+then
+    echo "Given output directory does not exist!"
+    usage
+    exit 1
+fi
+
+if [ $SHOULD_CLEAR ]
+then
+    clear_logs $OUTPUT_DIR $CORE_NUMBER
+fi
+
 echo "Running"
 while true
 do
@@ -56,6 +91,7 @@ do
     do
         copy_log_part $OUTPUT_DIR/log-current.txt $i $OUTPUT_DIR
     done
+    rm $OUTPUT_DIR/log-current.txt
 done
 
 exit 0
