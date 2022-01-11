4f419358f5ce68007b621452db5b6427e910a8c6
Maximilian Stiede <maximilian.stiede@student.hpi.de>
2022-01-11 18:09:57 +0100

    root user handling

--- make_reload.sh
+++ make_reload.sh
@@ -1,8 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 make
-cpupower frequency-set -g schedutil
-rmmod memutil.ko
-insmod memutil.ko
-cpupower frequency-set -g memutil
-
+if [ "$UID" -eq 0 ]; then
+    cpupower frequency-set -g schedutil
+    rmmod memutil.ko
+    insmod memutil.ko
+    cpupower frequency-set -g memutil
+else
+    echo "Run this script as root to refresh the kernel module"
+fi
